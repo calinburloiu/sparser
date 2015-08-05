b75790b2181fb26be77ed7520cfb67678dc3f5f2
Calin-Andrei Burloiu <calin.burloiu@avira.com>
2015-08-06 01:05:50 +0300

    Better val names.

--- macros/src/main/scala/com/avira/ds/sparser/FieldMatch.scala
+++ macros/src/main/scala/com/avira/ds/sparser/FieldMatch.scala
@@ -17,10 +17,12 @@ object FieldMatch {
       expectedFieldValue: c.Expr[Any]): c.Expr[FieldMatch[O]] = {
     import c.universe._
 
-    val funcBodyRepExpr = selectFieldToStringImpl(c)(selectField)
+    // Extract field definition from function selectField.
+    val selectFieldBodyRepExpr = selectFieldToStringImpl(c)(selectField)
 
+    // Generate AST of FieldMatch default apply with the extract fieldName (field definition).
     reify {
-      FieldMatch(funcBodyRepExpr.splice, selectField.splice, expectedFieldValue.splice)
+      FieldMatch(selectFieldBodyRepExpr.splice, selectField.splice, expectedFieldValue.splice)
     }
   }
 
@@ -29,17 +31,22 @@ object FieldMatch {
   def selectFieldToStringImpl[O](c: Context)(selectField: c.Expr[O => Any]): c.Expr[String] = {
     import c.universe._
 
-    val funcBodyRep = selectField.tree match {
-      case Function(_, s) =>
-        val showOutput = show(s)
-        // Remove prefix:
-        showOutput.substring(showOutput.indexOf('.') + 1)
-        // Remove ".apply":
+    // Extract field definition from function body.
+    val selectFieldBodyRep = selectField.tree match {
+      case Function(_, body) =>
+        val showOutput = show(body)
+
+        // Transform function body into a pretty field definition.
+        showOutput
+          // Remove prefix; e.g. x$1.field ==> field:
+          .substring(showOutput.indexOf('.') + 1)
+          // Remove ".apply"; e.g. friends.apply(2) ==> friends(2):
           .replace(".apply", "")
     }
-    val funcBodyRepTree = Literal(Constant(funcBodyRep))
-    val funcBodyRepExpr = c.Expr[String](funcBodyRepTree)
 
-    reify { funcBodyRepExpr.splice }
+    // Create AST for the field definition string.
+    val selectFieldBodyRepTree = Literal(Constant(selectFieldBodyRep))
+    val selectFieldBodyRepExpr = c.Expr[String](selectFieldBodyRepTree)
+    reify { selectFieldBodyRepExpr.splice }
   }
 }
