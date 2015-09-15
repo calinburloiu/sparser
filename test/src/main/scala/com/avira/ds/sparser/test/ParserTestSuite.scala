48a5796df5a9bb8d37eefeb9c45177fd17168180
Calin-Andrei Burloiu <calin.burloiu@avira.com>
2015-09-15 18:08:15 +0300

    Check mapOutput undefined values and document mapOutput feature.

--- test/src/main/scala/com/avira/ds/sparser/test/ParserTestSuite.scala
+++ test/src/main/scala/com/avira/ds/sparser/test/ParserTestSuite.scala
@@ -3,6 +3,8 @@ package com.avira.ds.sparser.test
 import com.avira.ds.sparser._
 import org.scalatest.WordSpec
 
+import scala.reflect.ClassTag
+
 /** Extend this class for testing a `com.avira.ds.sparser.Parser` based on
   * expected outcomes for different input value.
   *
@@ -48,7 +50,7 @@ import org.scalatest.WordSpec
   * @tparam O Output value type of the parser tested
   * @see [[ParserTestSuite#ParserTest]]
   */
-abstract class ParserTestSuite[I, O] extends WordSpec {
+abstract class ParserTestSuite[I, O : ClassTag] extends WordSpec {
   import ParserTestSuite._
 
   /** Instance of the parser you want to test */
@@ -62,6 +64,15 @@ abstract class ParserTestSuite[I, O] extends WordSpec {
     * descriptive name which should fit as ''<name>'' in phrase ''Parsing input
     * "<name>" should extract field "<field>"''. To define the expected outcome
     * for the input use one of the case classes of [[ExpectedResult]].
+    *
+    * You may want to transform (map) parser's actual output value before you
+    * match it. In order to to this you can use `mapOutput` partial function. A
+    * typical use case when you need to do this is when you want to "cast" the
+    * output value to a more specific class. Since [[ExpectedResult]] is
+    * invariant with respect to parser's (`Parser[I, O]`) output value (`O`) you
+    * can define a partial function such as
+    * `{ case o: MoreSpecificClass => o }` to do the cast, which will expose
+    * `MoreSpecificClass` members in `FieldMatch#selectField`function.
     *
     * @param name Short and descriptive name of the test case which will be
     * printed by ScalaTest when running the test suite
@@ -78,7 +89,8 @@ abstract class ParserTestSuite[I, O] extends WordSpec {
     import ParserTest._
 
     s"""Parsing input "$name"""" should {
-      val parseResult = parser.parse(input).transform { output =>
+      val parseResult = parser.parse(input)
+      val mappedParseResult = parseResult.transform { output =>
         if (mapOutput.isDefinedAt(output)) {
           TransformSuccess(mapOutput(output))
         } else {
@@ -86,27 +98,31 @@ abstract class ParserTestSuite[I, O] extends WordSpec {
         }
       }
 
-      // TODO Treat UndefinedFunctionDomain case.
-
-      val ParseResult(actualValueOption, errors, _) = parseResult
+      val ParseResult(actualValueOption, errors, _) = mappedParseResult
       val errorClasses = errors.map(_.getClass).toSet
+
+      if (mappedParseResult.errors.contains(UndefinedFunctionDomain)) {
+        fail("mapOutput partial function is not defined on the value extracted by parsing input " +
+            s""""$name", ${parseResult.valueOption.get}.""")
+      }
 
       // Check if ExpectedResult case classes match their corresponding ParseResult case classes.
       "lead to the same result type" in {
-        (expectedResult, parseResult) match {
+        (expectedResult, mappedParseResult) match {
           case (ExpectedSuccessResult(_), ParseResult.Success(_, _)) => assert(true)
           case (ExpectedWarningResult(_, _), ParseResult.Warning(_, _, _)) => assert(true)
           case (ExpectedFailureResult(_), ParseResult.Failure(_, _)) => assert(true)
           case _ => assert(false,
-            s"expected ${getResultTypeName(expectedResult)} result, but " +
-                s"${getResultTypeName(parseResult)} was found")
+            s"Expected ${getResultTypeName(expectedResult)} result, but " +
+                s"${getResultTypeName(mappedParseResult)} was found with error(s) " +
+                s"${mappedParseResult.errors.map(_.getClass.getName).mkString(", ")}.")
         }
       }
 
       // Check field values.
       actualValueOption.foreach { actualValue =>
         val fieldValues = expectedResult.expectedValueOption
-          .fold(Seq[FieldMatch[OO]]())(_.fieldValues)
+            .fold(Seq[FieldMatch[OO]]())(_.fieldValues)
         for (FieldMatch(fieldName, selectField, expectedFieldValue) <- fieldValues) {
           s"""extract field $fieldName""" in {
             assert(
@@ -132,6 +148,7 @@ abstract class ParserTestSuite[I, O] extends WordSpec {
   }
 
   object ParserTest {
+
     def apply(
         name: String,
         input: I,
