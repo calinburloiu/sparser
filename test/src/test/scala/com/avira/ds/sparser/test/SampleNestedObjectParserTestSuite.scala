08bedf120293ac3f67741800b4e3c6460c2f65c6
Calin-Andrei Burloiu <calin.burloiu@avira.com>
2015-09-16 12:02:23 +0300

    Add test case for a mapOutput that goes deep into a nested object.

--- test/src/test/scala/com/avira/ds/sparser/test/SampleNestedObjectParserTestSuite.scala
+++ test/src/test/scala/com/avira/ds/sparser/test/SampleNestedObjectParserTestSuite.scala
@@ -1,7 +1,7 @@
 package com.avira.ds.sparser.test
 
 import com.avira.ds.sparser.Parser
-import com.avira.ds.sparser.samples.{SampleNestedObjectParser, NestedObject}
+import com.avira.ds.sparser.samples.{Ratio, SampleNestedObjectParser, NestedObject}
 
 /** Test suite example for [[com.avira.ds.sparser.samples.SampleNestedObjectParser]]. */
 class SampleNestedObjectParserTestSuite extends ParserTestSuite[String, NestedObject] {
@@ -16,6 +16,17 @@ class SampleNestedObjectParserTestSuite extends ParserTestSuite[String, NestedOb
         FieldMatch(_.b, "Andrei"),
         FieldMatch(_.c.x, 5),
         FieldMatch(_.c.y, 4)
+      )
+    )
+  )
+
+  ParserTest[Ratio]("partial function goes deep",
+    input="Calin\tAndrei\t5:4",
+    { case NestedObject(_, _, c) => c },
+    ExpectedSuccessResult(
+      ExpectedValue(
+        FieldMatch(_.x, 5),
+        FieldMatch(_.y, 4)
       )
     )
   )
